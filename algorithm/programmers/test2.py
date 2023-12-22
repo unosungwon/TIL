145f469f4618d2dc4cd781328d537a7f1e0fabe4
SUNGWON <amyksw@naver.com>
2023-12-22 09:13:45 +0900

    알고리즘 수정 및 폴더 삭제

--- algorithm/programmers/test2.py
+++
@@ -1,9 +0,0 @@
-def solution(d, budget):
-    for i in range(1, len(d)):
-        sort_d = sorted(d)
-        if sum(sort_d[0:i]) > budget:
-            break
-
-    return i - 1
-
-print(solution([1, 3, 2, 5, 4], 9))
